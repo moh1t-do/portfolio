7a6418cd7cc3944e3d31d6f225ac07c00b275385
mohit <mohitd2288@gmail.com>
2023-03-26 20:58:56 +0530

    changed card styles

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -20,31 +20,33 @@ function Tile(props) {
         }
     }, [control, inView])
 
-    const { image, title, top, link } = props
+    const { image, title, link, gitLink } = props
     return (
         <motion.div
             id="projects"
             ref={ref}
             variants={boxVariant}
             animate={control}
-            className={`flex flex-col justify-center pt-1 drop-shadow ${
-                top ? 'h-96' : 'h-80 sm:h-[26rem] md:h-[30rem] lg:h-72'
-            }`}
+            className="flex h-80 flex-col justify-center pt-1 drop-shadow sm:h-[26rem] md:h-[30rem] lg:h-72"
         >
             <div
                 style={{ backgroundImage: `url(${image})` }}
-                className="bg relative flex-auto bg-cover bg-center bg-no-repeat text-4xl text-white"
+                className="group relative flex-auto bg-cover bg-center bg-no-repeat text-4xl text-white"
             >
-                <div className="h-full w-full bg-black opacity-10 transition-all ease-in hover:opacity-5"></div>
-                {top && (
-                    <div className="absolute top-1/2 flex w-full justify-center font-bold">
+                <div className="h-full w-full bg-black opacity-10 transition-all ease-in group-hover:opacity-80"></div>
+                <div className="absolute top-1/2 flex w-full justify-center font-bold opacity-0 hover:text-primary group-hover:opacity-100">
+                    <a href={link} target="_blank">
                         {title}
-                    </div>
-                )}
+                    </a>
+                </div>
             </div>
             <div className="py-1 text-sm sm:flex sm:justify-between">
-                <a href={link} target="_blank" className="hover:text-primary">
-                    LINK
+                <a
+                    href={gitLink}
+                    target="_blank"
+                    className="cursor-pointer hover:text-primary"
+                >
+                    GITHUB LINK
                 </a>
             </div>
         </motion.div>
@@ -58,13 +60,13 @@ function Projects() {
                 Projects
             </h1>
             <div className="grid w-full grid-cols-1 grid-rows-1 gap-6 px-5 pt-20 md:px-20 lg:grid-cols-3">
-                {projectsData.map(({ key, image, title, subtitle, link }) => (
+                {projectsData.map(({ key, image, title, link, gitLink }) => (
                     <Tile
                         key={key}
                         image={image}
                         title={title}
-                        subtitle={subtitle}
                         link={link}
+                        gitLink={gitLink}
                     />
                 ))}
             </div>
